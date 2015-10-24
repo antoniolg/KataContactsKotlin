9120ef8d6997f9adc668f6699eb93e79b414055f
Antonio Leiva <antonioleivag@gmail.com>
2015-10-24 23:26:40 +0200

    Add ContactsListPresenter.View implementation

---
+++ src/main/java/com/antonioleiva/kataagenda/ui/SysOutContactsListView.kt
@@ -0,0 +1,22 @@
+package com.antonioleiva.kataagenda.ui
+
+import com.antonioleiva.kataagenda.Contact
+
+class SysOutContactsListView : ContactsListPresenter.View {
+
+    override fun showWelcomeMessage() = Unit
+
+    override fun showGoodbyeMessage() = Unit
+
+    override fun getNewContactFirstName(): String = ""
+
+    override fun getContactLastName(): String = ""
+
+    override fun getNewContactPhoneNumber(): String = ""
+
+    override fun showContacts(contactList: List<Contact>) = Unit
+
+    override fun showDefaultError() = Unit
+
+    override fun showEmptyCase() = Unit
+}
