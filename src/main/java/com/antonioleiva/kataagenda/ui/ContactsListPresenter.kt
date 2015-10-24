edcb43f60d88fd2ca71b6f9f611bd4a8f07e4a31
Antonio Leiva <antonioleivag@gmail.com>
2015-10-24 23:21:46 +0200

    Implement ContactsListPresenter

--- src/main/java/com/antonioleiva/kataagenda/ui/ContactsListPresenter.kt
+++ src/main/java/com/antonioleiva/kataagenda/ui/ContactsListPresenter.kt
@@ -1,11 +1,50 @@
 package com.antonioleiva.kataagenda.ui
 
+import com.antonioleiva.kataagenda.Contact
 import com.antonioleiva.kataagenda.common.ui.Presenter
+import com.antonioleiva.kataagenda.usecases.AddContact
+import com.antonioleiva.kataagenda.usecases.GetContacts
 
-class ContactsListPresenter(view: ContactsListPresenter.View) : Presenter<ContactsListPresenter.View>(view) {
+class ContactsListPresenter(view: ContactsListPresenter.View, val addContact: AddContact, val getContacts: GetContacts) :
+        Presenter<ContactsListPresenter.View>(view) {
 
-    override fun onInitialize() = Unit
-    override fun onStop() = Unit
+    override fun onInitialize() {
+        view.showWelcomeMessage()
+        loadContactsList()
+    }
 
-    interface View : Presenter.View
+    override fun onStop() = view.showGoodbyeMessage()
+
+    fun onAddContactOptionSelected() {
+        val contactToAdd = requestNewContact()
+        addContact(contactToAdd)
+        loadContactsList()
+    }
+
+    private fun requestNewContact(): Contact {
+        val firstName = view.getNewContactFirstName()
+        val lastName = view.getContactLastName()
+        val phoneNumber = view.getNewContactPhoneNumber()
+        return Contact(firstName, lastName, phoneNumber)
+    }
+
+    private fun loadContactsList() {
+        val contactList = getContacts()
+        view.showContacts(contactList)
+    }
+
+    interface View : Presenter.View {
+
+        fun showWelcomeMessage()
+
+        fun showGoodbyeMessage()
+
+        fun getNewContactFirstName(): String
+
+        fun getContactLastName(): String
+
+        fun getNewContactPhoneNumber(): String
+
+        fun showContacts(contactList: List<Contact>)
+    }
 }
