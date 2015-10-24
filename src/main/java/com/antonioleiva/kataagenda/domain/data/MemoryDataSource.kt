b3d59f7b8be468da67a12e7e25ca3951fe092738
Antonio Leiva <antonioleivag@gmail.com>
2015-10-24 23:35:49 +0200

    Generify MemoryDataSource

--- src/main/java/com/antonioleiva/kataagenda/domain/data/MemoryDataSource.kt
+++ src/main/java/com/antonioleiva/kataagenda/domain/data/MemoryDataSource.kt
@@ -1,14 +1,13 @@
 package com.antonioleiva.kataagenda.domain.data
 
-import com.antonioleiva.kataagenda.domain.Contact
 import com.antonioleiva.kataagenda.common.data.DataSource
 
-class MemoryDataSource : DataSource<Contact> {
+class MemoryDataSource<T> : DataSource<T> {
 
-    private var data = emptyList<Contact>()
+    private var data = emptyList<T>()
 
-    override fun getData(): List<Contact> = data
+    override fun getData(): List<T> = data
 
-    override fun add(item: Contact): Contact = item.apply { data += this }
+    override fun add(item: T): T = item.apply { data += this }
 
 }
